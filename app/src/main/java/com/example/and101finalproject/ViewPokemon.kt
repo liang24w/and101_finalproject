2b291e926cdcc380a05926a92a17ea9fc7e26483
Grace-Shao <gshao8@gatech.edu>
2024-04-16 22:05:42 -0400

    3 screens, next btn to navigate btwn them

--- app/src/main/java/com/example/and101finalproject/ViewPokemon.kt
+++ app/src/main/java/com/example/and101finalproject/ViewPokemon.kt
@@ -5,21 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class ViewPokemon : AppCompatActivity() {
     private lateinit var nextBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_view_pokemon)
         nextBtn = findViewById(R.id.nextBtn)
 
         // Set OnClickListener outside onCreate
         nextBtn.setOnClickListener {
             // Define actions to perform when the button is clicked
             // For example, start a new activity:
-            val intent = Intent(this, ViewPokemon::class.java)
+            val intent = Intent(this, AdoptPokemonForm::class.java)
             startActivity(intent)
         }
     }
-
-
 }
