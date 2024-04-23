0073f95b02ca5fd1997d35b9f724884d4dd7ab57
YousufTheSWE <yousufh2006@gmail.com>
2024-04-22 20:22:19 -0400

    Fixed Next Button

--- app/src/main/java/com/example/and101finalproject/MainActivity.kt
+++ app/src/main/java/com/example/and101finalproject/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.and101finalproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,87 +13,19 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var pokeList: MutableList<String>
-    private lateinit var rvPokes: RecyclerView
-    private lateinit var adapter: PokeAdapter
-    private lateinit var pokeNameList: MutableList<String>
-    private lateinit var pokeIdList: MutableList<String>
-    private lateinit var pokeDescList: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rvPokes = findViewById(R.id.poke_list)
-        pokeList = mutableListOf()
-        pokeNameList = mutableListOf()
-        pokeIdList = mutableListOf()
-        pokeDescList = mutableListOf()
-        adapter = PokeAdapter(pokeList, pokeNameList, pokeIdList, pokeDescList)
-
-        rvPokes.adapter = adapter
-        rvPokes.layoutManager = LinearLayoutManager(this@MainActivity)
-        rvPokes.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
-
-        getPokemonImageArray()
-    }
-
-    private fun getPokemonImageArray() {
-        val client = AsyncHttpClient()
-        val client2 = AsyncHttpClient()
-        for (i in 0 until 10) {
-            val url = "https://pokeapi.co/api/v2/pokemon/$i"
-            val descurl = "https://pokeapi.co/api/v2/pokemon-species/$i"
-
-            client.get(url, object : JsonHttpResponseHandler() {
-                override fun onFailure(
-                    statusCode: Int,
-                    headers: Headers?,
-                    errorResponse: String,
-                    throwable: Throwable?
-                ) {
-                    Log.d("Pokemon Error", errorResponse)
-                }
-
-                override fun onSuccess(
-                    statusCode: Int,
-                    headers: Headers?,
-                    pokejson: JsonHttpResponseHandler.JSON
-                ) {
-                    val pokeImageUrl =
-                        pokejson.jsonObject.getJSONObject("sprites").getString("front_default")
-                    val pokeName = pokejson.jsonObject.getString("name").capitalize()
-                    val pokeId = pokejson.jsonObject.getJSONArray("abilities").getJSONObject(0)
-                        .getJSONObject("ability").getString("name")
-
-                    pokeIdList.add(pokeId)
-                    pokeNameList.add(pokeName)
-                    pokeList.add(pokeImageUrl)
-
-                    adapter.notifyDataSetChanged()
-                }
-            }) //end client
-
-            client2.get(descurl, object : JsonHttpResponseHandler() {
-                override fun onFailure(
-                    statusCode: Int,
-                    headers: Headers?,
-                    response: String,
-                    throwable: Throwable?
-                ) {
-                    Log.d("Pokemon Description Error", response)
-                }
-
-                override fun onSuccess(statusCode: Int, headers: Headers?, json: JsonHttpResponseHandler.JSON) {
-                    val pokeDesc = json.jsonObject.getJSONArray("flavor_text_entries").getJSONObject(0).getString("flavor_text")
-
-                    pokeDescList.add(pokeDesc)
-
-                    adapter.notifyDataSetChanged()
-                }
-
-            }) //end client2
+        findViewById<Button>(R.id.nextBtn).setOnClickListener() {
+            val intent = Intent(this, ViewPokemon::class.java)
+            startActivity(intent)
         }
+
     }
+
+
 
 
 }
